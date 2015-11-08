a1314577b61ecdb8fa2995c0bfa463d35b0a9bd4
Wellington Soares <well.cco@gmail.com>
2015-11-08 06:12:01 -0800

    New CheckController

---
+++ StoryBoardTest/CheckController.h
@@ -0,0 +1,13 @@
+//
+//  CheckController.h
+//  ExecTravel
+//
+//  Created by Wellington Soares on 11/8/15.
+//  Copyright © 2015 livani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckController : UIViewController
+
+@end
