7beb6166bb9fc6948984d5eb56278c0ee321d390
Livani Pillay <livanipillay@gmail.com>
2015-11-07 22:23:51 -0800

    Added mappings for

--- StoryBoardTest/SelectFlightViewController.h
+++ StoryBoardTest/SelectFlightViewController.h
@@ -10,4 +10,25 @@
 
 @interface SelectFlightViewController : UIViewController
 
+@property (nonatomic,strong) IBOutlet UILabel *flightNumber1;
+@property (nonatomic,strong) IBOutlet UILabel *departureDate1;
+@property (nonatomic,strong) IBOutlet UILabel *departureTime1;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalDate1;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalTime1;
+@property (nonatomic,strong) IBOutlet UILabel *seatAvailable1;
+
+@property (nonatomic,strong) IBOutlet UILabel *flightNumber2;
+@property (nonatomic,strong) IBOutlet UILabel *departureDate2;
+@property (nonatomic,strong) IBOutlet UILabel *departureTime2;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalDate2;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalTime2;
+@property (nonatomic,strong) IBOutlet UILabel *seatAvailable2;
+
+@property (nonatomic,strong) IBOutlet UILabel *flightNumber3;
+@property (nonatomic,strong) IBOutlet UILabel *departureDate3;
+@property (nonatomic,strong) IBOutlet UILabel *departureTime3;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalDate3;
+@property (nonatomic,strong) IBOutlet UILabel *arrivalTime3;
+@property (nonatomic,strong) IBOutlet UILabel *seatAvailable3;
+
 @end
